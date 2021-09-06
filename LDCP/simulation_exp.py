54ca08aabf5fbf552a02891d2fa3ea53a07a685b
aaronhan223 <aaronhan223@gmail.com>
2021-09-05 19:20:41 -0500

    udpate simulation function

--- LDCP/simulation_exp.py
+++ LDCP/simulation_exp.py
@@ -10,6 +10,38 @@ from tqdm import tqdm
 import pdb
 
 
+class simulation:
+    """ Functions for generating 1 dimensional simulation
+    Parameters
+    ----------
+    rank: int, the number of simulation function
+    """
+    def __init__(self, rank) -> None:
+        self.rank = rank
+
+    def f_1(self, x):
+        return np.sin(x) ** 2 + 0.1 + 0.6 * np.sin(2 * x) * np.random.randn(1)
+
+    def f_2(self, x):
+        return 2 * np.sin(x) ** 2 + 0.1 + 0.15 * x * np.random.randn(1)
+
+    def f_3(self, x):
+        x = np.random.poisson(np.sin(x) ** 2 + 0.1) + 0.08 * x * np.random.randn(1)
+        x += 25 * (np.random.uniform(0, 1, 1) < 0.01) * np.random.randn(1)
+        return x
+
+    def generate(self, data):
+        y = 0 * data
+        for i in range(len(data)):
+            if self.rank == 1:
+                y[i] = self.f_1(data[i])
+            elif self.rank == 2:
+                y[i] = self.f_2(data[i])
+            else:
+                y[i] = self.f_3(data[i])
+        return y.astype(np.float32)
+
+
 def plot_func(x,
               y,
               y_u=None,
@@ -75,18 +107,6 @@ def plot_func(x,
         plt.savefig(filename, bbox_inches='tight', dpi=300)
     plt.close()
 
-
-def f(x):
-    ''' Construct data (1D example)
-    '''
-    ax = 0*x
-    for i in range(len(x)):
-        # ax[i] = np.sin(x[i])**2+0.1 + 0.6*np.sin(2*x[i])*np.random.randn(1)
-        # ax[i] = 2*np.sin(x[i])**2+0.1 + 0.15*x[i]*np.random.randn(1)
-        ax[i] = np.random.poisson(np.sin(x[i])**2+0.1) + 0.08*x[i]*np.random.randn(1)
-        ax[i] += 25*(np.random.uniform(0,1,1)<0.01)*np.random.randn(1)
-    return ax.astype(np.float32)
-
 split_color = 'tomato'
 local_color = 'gray'
 cqr_color = 'lightblue'
@@ -123,8 +143,9 @@ x_train = np.random.uniform(0, 5.0, size=n_train).astype(np.float32)
 x_test = np.random.uniform(0, 5.0, size=n_test).astype(np.float32)
 
 # generate labels
-y_train = f(x_train)
-y_test = f(x_test)
+sim = simulation(3)
+y_train = sim.generate(x_train)
+y_test = sim.generate(x_test)
 
 # reshape the features
 x_train = np.reshape(x_train,(n_train,1))
