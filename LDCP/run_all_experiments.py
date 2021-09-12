64e678b1e50483b990cb42014f291fe2d233c391
aaronhan223 <aaronhan223@gmail.com>
2021-09-12 02:04:39 -0500

    update experiments

--- LDCP/run_all_experiments.py
+++ LDCP/run_all_experiments.py
@@ -3,15 +3,22 @@ import config
 
 
 if __name__ == '__main__':
-    # TODO: 1. add local conformal, 2. add and test real data from original dataset.py file, 3. assymmetric quantile error function
+
     if config.UtilsParams.experiment == 'prediction':
         dataset_list = ['simulation_1', 'simulation_2', 'simulation_3', 'star', 'facebook_1', 'facebook_2', 'bio', 'blog_data', 'bike', 'concrete', 'community']
-        model_list = ['random_forest', 'linear_regression', 'neural_net']
-        method_name = ['ldcp', 'cqr', 'cqr-asy', 'split', 'lacp']
-        run_pred_experiment(dataset_name='bio', model_name='linear_regression', method_name='ldcp', random_seed=config.UtilsParams.seed)
-        run_pred_experiment(dataset_name='bio', model_name='linear_regression', method_name='cqr', random_seed=config.UtilsParams.seed)
-        run_pred_experiment(dataset_name='bio', model_name='linear_regression', method_name='lacp', random_seed=config.UtilsParams.seed)
-        run_pred_experiment(dataset_name='bio', model_name='linear_regression', method_name='split', random_seed=config.UtilsParams.seed)
+        model_list = ['random_forest', 'linear', 'neural_net']
+        method_name = ['ldcp', 'cqr', 'cqr-asy', 'split', 'lacp', 'qr']
+
+        for data in dataset_list:
+            for model in model_list:
+                for method in method_name:
+                    is_cp = False if method == 'qr' else True
+                    run_pred_experiment(dataset_name=data, 
+                                        model_name=model, 
+                                        method_name=method, 
+                                        random_seed=config.UtilsParams.seed,
+                                        conformal=is_cp)
+
     if config.UtilsParams.experiment == 'cov_shift':
         run_cov_shift()
     if config.UtilsParams.experiment == 'model_bias':
